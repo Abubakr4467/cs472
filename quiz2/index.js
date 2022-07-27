c541f5a794fbed63763517e1d55f84ba73c2f5a6
Abubakr Abdullaev <52697855+Abubakr4467@users.noreply.github.com>
2022-07-27 14:48:08 -0500

    Add files via upload
    quiz 2

---
+++ quiz2/index.js
@@ -0,0 +1,27 @@
+
+
+
+
+// $("li").each(function (idx,e ){
+//     $(e).css("color", "yellow");
+// });
+
+// $("li").each(function(){
+//     $(this).css("color", "green");
+// });
+
+
+// $("li").each(function(idx){
+//     $(this).css("color", "red");
+// });
+
+
+$(document).ready(function(){
+    $("div ul").css("color","blue");
+});
+
+
+$(document).ready(function(){
+    $("body").append("<div><h1>JQuery Core</h1></div>");
+
+});
